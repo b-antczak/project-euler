54bd90dd225a5dc9bef7812fb9066b5066d56152
Bartosz <bantczak@uwaterloo.ca>
2020-04-04 21:02:15 -0400

    cleanup p6

--- p6/p6.js
+++ p6/p6.js
@@ -1,4 +1,8 @@
 const sumPowersUpTo = (n, p) => [...new Array(n)].reduce((acc, _, i) => acc + Math.pow(i + 1, p), 0);
 
-const n = 100;
-console.log(Math.abs(Math.pow(sumPowersUpTo(n, 1), 2) - sumPowersUpTo(n, 2)));
+function p6() {
+  const n = 100;
+  return Math.abs(Math.pow(sumPowersUpTo(n, 1), 2) - sumPowersUpTo(n, 2));
+}
+
+console.log(p6());
